6721b6adbb3876ebfda2da2af4060bf118070339
alizaLakhani14 <49196299+alizaLakhani14@users.noreply.github.com>
2019-06-20 15:54:39 +0500

    Add files via upload

---
+++ 20-jun-19/script.js
@@ -0,0 +1,21 @@
+// function checkSpeed(){
+//     let maxSpeed = document.querySelector("input").value;
+//      if (maxSpeed > 60 ){
+//          alert("Overspeeding");
+//      }
+
+//      else{
+//          alert("Enjoy your journey");
+//      }
+// }
+
+// function login(){
+//     let loggedIn = document.querySelector("a").visited;
+//      if (loggedIn == true){
+//          console.log("Welcome,user");
+//      }
+
+//      else{
+//          console.log("Please log in to continue");
+//      }
+// }
