75c14014ba65dcace0a7566755fdd2855a5c24df
alizaLakhani14 <49196299+alizaLakhani14@users.noreply.github.com>
2019-07-11 14:56:12 +0500

    Create script.js

---
+++ Database/script.js
@@ -0,0 +1,25 @@
+let contacts = [
+    {
+        name: "armia",
+        isRead: true,
+        profilePicture: "#",
+        firstLine: "yes\,okay",
+        number:033-8888888
+    }
+    {
+        name: "narjis",
+        isRead: False,
+        no\.OfUnreadMessages: 2,
+        profilePicture: "#",
+        firstLine: "coming",
+        number:033-7777777
+    }
+    {
+        name: "majeda",
+        isRead: true,
+        profilePicture: "#",
+        firstLine: "okay",
+        number:033-555555
+    }
+
+]
