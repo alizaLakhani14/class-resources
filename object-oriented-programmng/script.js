6f372d80957c0a838ceb58739bbca91e4ad60dd4
alizaLakhani14 <49196299+alizaLakhani14@users.noreply.github.com>
2019-08-27 18:07:55 +0500

    Update script.js

--- object-oriented-programmng/script.js
+++ object-oriented-programmng/script.js
@@ -1,1 +1,57 @@
+// let constructPerson = (personName, personGender, hobbies, meal) => {
+//     return {
+//         name : personName,
+//         gender : personGender,
+//         hobbies : hobbies,
+//         eat : function () {
+//             console.log(meal)
+//         },
+//         greet : function () {
+//             console.log(`Hello ${this.name}`)
+//         }
+
+//     }
+
+// }
+// let personA = constructPerson("Aliza", "female", ["playing"], "biryani");
+
+// let makeIdCard = function (name, age) {
+//     let student = {
+//         name: name,
+//         age: age,
+//         interests: [],
+//         bio: function bio() {
+//             console.log(`My name is ${this.name}.`)
+//         }
+//     }
+//     return student;
+
+// }
+
+// function startCreatingIdCards() {
+//     let names = ["Aliza", "Armia", "Arfa"];
+//     let ages = [20, 21, 22];
+//     let persons = []
+//     for(let i=0; i < 3; i++){
+//         let person = makeIdCard(names[i], ages[i]);
+//         persons.push(person);
+//     }
+//  return persons;
+// }                                              
+
+
+
+// let makeCar = (name, color, model, owner) => {
+//     let car = {
+//         name: name,
+//         colour: colour,
+//         model: model,
+
+//         ownedBy: function () {
+//             console.log(`I am owned by ${this.owner}`);
+//         }
+//     }
+//     return car;
+// }
+
 
